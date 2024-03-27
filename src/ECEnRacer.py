5a15273335d7acc7c1d955d49384fb9f0df601b7
Daniel Cheney <daniel.cheney01@gmail.com>
2024-03-27 14:52:00 -0600

    updating client/server structure

--- src/ECEnRacer.py
+++ src/ECEnRacer.py
@@ -18,91 +18,55 @@ From the Realsense camera:
 
 	EXTREMELY IMPORTANT: Read the user manual carefully before operate the car
 
+	# Use $ ls /dev/tty* to find the serial port connected to Arduino
+
 	# If you get cannot get frame error: use 'sudo pkill -9 python3.6' and wait 15 seconds
 **************************************
 '''
 
 # import the necessary packages
-from Arduino import Arduino
+from hardware.Arduino import Arduino
 from RealSense import *
 import cv2
-import imagezmq
-import zmq
-import threading
+import pickle
+import socket
 # from simple_pid import PID
 
-enableDepth = False
-rs = RealSense(RS_VGA, enableDepth)		# RS_VGA, RS_720P, or RS_1080P
-
-# Use $ ls /dev/tty* to find the serial port connected to Arduino
-# Car = Arduino("/dev/ttyUSB0", 115200)                # Linux
-#Car = Arduino("/dev/tty.usbserial-2140", 115200)    # Mac
-time.sleep(3)
-
-# Car.zero(1440)      # Set car to go straight.  Change this for your car.
-# Car.pid(1)          # Use PID control
-
+# set up the camera
+rs = RealSense(RS_VGA)
 (time_, rgb, depth, accel, gyro) = rs.getData()
-def sender_start(connect_to=None):
-    sender = imagezmq.ImageSender(connect_to=connect_to)
-    # sender.zmq_socket.setsockopt(zmq.LINGER, 0)  # prevents ZMQ hang on exit
-    # # NOTE: because of the way PyZMQ and imageZMQ are implemented, the
-    # #       timeout values specified must be integer constants, not variables.
-    # #       The timeout value is in milliseconds, e.g., 2000 = 2 seconds.
-    # sender.zmq_socket.setsockopt(zmq.RCVTIMEO, 2000)  # set a receive timeout
-    # sender.zmq_socket.setsockopt(zmq.SNDTIMEO, 2000)  # set a send timeout
-    return sender
+print("Camera is ready...")
 
-server_address = 'tcp://10.32.114.243:5555'
-sender = sender_start(server_address)
-host_name = "LightningMcQueen"
-print("Started Client")
+# set up the Car
+Car = Arduino("/dev/ttyUSB0", 115200)                # Linux
+time.sleep(3)
+Car.zero(1440)      # Set car to go straight.  Change this for your car.
+Car.pid(1)          # Use PID control
+print("Car is ready...")
 
-# ## SETUP PID Controller
-# pid = PID()
-# pid.Ki = -.01*0
-# pid.Kd = -.01*0
-# pid.Kp = -30/250 #degrees per pixel
-# frameUpdate = 1
-# pid.sample_time = frameUpdate/30.0
-# pid.output_limits = (-30,30)
-# desXCoord = rgb.shape[0]*3/5
-# pid.setpoint = desXCoord
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1000000)
+server_ip = "10.32.114.243"
+server_port = 5555
+print("Client is ready...")
 
-# i = 1
-# angle = 0
-# FAST_SPEED = 1.3
-# SLOW_SPEED = 0.5
-# speed = FAST_SPEED
-# Car.drive(FAST_SPEED)
+Car.drive(3.0)
 
-
-# # You can use kd and kp commands to change KP and KD values.  Default values are good.
-# # loop over frames from Realsense
-j = 0
-
-
-start = time.time()
 while(True):
 	loopStart = time.time()
 	(time_, img, _, accel, gyro) = rs.getData()
 
-	smaller_img = cv2.resize(img, (0, 0), fx=0.25, fy=0.25)
+	# Send image to server
+	ret, buffer = cv2.imencode(".jpg", img, [int(cv2.IMWRITE_JPEG_QUALITY), 30])
+	x_as_bytes = pickle.dumps(buffer)
+	s.sendto((x_as_bytes), (server_ip, server_port))
 
-	if time.time() - start > 0.1:
-		# try:
-		reply_from_server = sender.send_image(host_name, smaller_img)
-		# except (zmq.ZMQError, zmq.ContextTerminated, zmq.Again):
-			# startClose = time.time()
-			# if 'sender' in locals():
-			# 	print('Closing ImageSender.')
-			# 	sender.close()
-			# print('Restarting ImageSender.')
-			# sender = sender_start(server_address)
-			# print(f"Reconnect time: {time.time() - startClose} s\n\n")
-		start = time.time()
+	# Get steering command from server
+	steerCmdBytes = s.recvfrom(1000000)
+	print(steerCmdBytes)
 
-
-		# print(reply_from_server)
+	# Do some steering
+	# Car.steer(steerCmd)
+	
 	print(f"Loop Time: {time.time()- loopStart} s")
 
--- src/server.py
+++ src/server.py
@@ -1,19 +1,49 @@
-# run this program on the Mac to display image streams from multiple RPis
-import cv2
-import imagezmq
-from ultralytics import YOLO
 
-image_hub = imagezmq.ImageHub()
-model = YOLO('./segmentation/runs/segment/train2/weights/best.pt')  # Load a model
-print("Server started. Waiting for images...")
+from ultralytics import YOLO
+import cv2
+import socket
+import pickle
+
+model = YOLO('./segmentation/runs/segment/train2/weights/best.pt')
+
+# Set up server socket
+s = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
+ip = "10.32.114.243"
+port = 5555
+s.bind((ip,port))
+print("Server is started")
+
+# Set up PID Controller 
+# pid = PID()
+# pid.Ki = -.01*0
+# pid.Kd = -.01*0
+# pid.Kp = -30/250 #degrees per pixel
+# frameUpdate = 1
+# pid.sample_time = frameUpdate/30.0
+# pid.output_limits = (-30,30)
+# desXCoord = rgb.shape[0]*3/5
+# pid.setpoint = desXCoor
 
 j = 0
-while True:  # show streamed images until Ctrl-C
-    rpi_name, image = image_hub.recv_image()
-    # cv2.imshow(rpi_name, image) # 1 window for each RPi
-    result = model.predict(source=image, show=True, verbose=False)
+while True:
+    # Receive message from client
+    client_msg = s.recvfrom(1000000)
+    client_ip = client_msg[1][0]
+    img_bytes = client_msg[0]
+    img_bytes_loaded = pickle.loads(img_bytes)
+    img = cv2.imdecode(img_bytes_loaded, cv2.IMREAD_COLOR) # possibly unnecessary
 
-    # cv2.waitKey(1)
+    # Get drivable area from image
+    result = model.predict(source=img, show=True, verbose=False)
 
-    image_hub.send_reply(b'OK')
+    # Calculate steering angle
+    steerCmd = j
+
+    # Send steering angle to client
+    s.sendto(str(steerCmd).encode(), (client_ip, port))
+
     j += 1
+    cv2.imshow('server', img)
+    if cv2.waitKey(10) == 13:
+        break
+cv2.destroyAllWindows()

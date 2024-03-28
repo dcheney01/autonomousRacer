8524b5bfd3b62512569567de3adc924b2711df5b
Daniel Cheney <daniel.cheney01@gmail.com>
2024-03-28 11:58:10 -0600

    updating server to handle errors better

--- src/server.py
+++ src/server.py
@@ -4,50 +4,78 @@ import cv2
 import socket
 import pickle
 import time
+from simple_pid import PID
+from path_planner import PathPlanner
+import numpy as np
 
 model = YOLO('./segmentation/runs/segment/train2/weights/best.pt')
 
 # Set up server socket
 s = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
 receive_buffer_size = 25000
-s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 36)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 72)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, receive_buffer_size)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setblocking(False)
+
+s.settimeout(0.1)
 ip = "10.32.114.243"
 port = 5555
 s.bind((ip,port))
 print("Server is started...")
 
 # Set up PID Controller 
-# pid = PID()
-# pid.Ki = -.01*0
-# pid.Kd = -.01*0
-# pid.Kp = -30/250 #degrees per pixel
-# frameUpdate = 1
-# pid.sample_time = frameUpdate/30.0
-# pid.output_limits = (-30,30)
-# desXCoord = rgb.shape[0]*3/5
-# pid.setpoint = desXCoor
-
-j = 0
+pid = PID()
+pid.Ki = 0.0
+pid.Kd = 0.000
+pid.Kp = -20/320 #degrees per pixel
+frameUpdate = 1
+pid.sample_time = frameUpdate/30.0
+pid.output_limits = (-30,30)
+desXCoord = 640 * 1/2
+pid.setpoint = desXCoord
+path_planner = PathPlanner(display=False)
 
 while True:
-    # Receive message from client
-    client_msg, client_addr = s.recvfrom(receive_buffer_size)
-    start = time.time()
-    client_ip = client_addr[0]
-    img_array = pickle.loads(client_msg)
-    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR) # possibly unnecessary
+    try:
+        # Receive message from client
+        client_msg, client_addr = s.recvfrom(receive_buffer_size)
+        start = time.time()
+        client_ip = client_addr[0]
+        img_array = pickle.loads(client_msg)
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR) # possibly unnecessary
 
-    # Get drivable area from image
-    result = model.predict(source=img, show=True, verbose=False)
+        # Get drivable area from image
+        result = model.predict(source=img, show=False, verbose=False)
+        try:
+            mask_img = result[0].masks.data[0].cpu().numpy()
+            if mask_img is None:
+                raise Exception("No mask image detected")
+        except Exception as e:
+            print(e)
+            s.sendto("0".encode(), client_addr)
+            continue
 
-    # Calculate steering angle
-    steerCmd = j
+        # Calculate steering angle
+        curr_x_coord = path_planner.get_line_to_follow(mask_img)
+        steerCmd = int(pid(curr_x_coord))
+        print(f"Current X Coord: {curr_x_coord}")
+        print(f"Steering Command: {steerCmd}\n")
 
-    # Send steering angle to client\
-    # print((str(steerCmd).encode()).__sizeof__())
-    s.sendto(str(steerCmd).encode(), client_addr)
+        # Send steering angle to client\
+        # print((str(steerCmd).encode()).__sizeof__())
+        s.sendto(str(steerCmd).encode(), client_addr)
 
-    j += 1
-    # cv2.imshow('server', img)
-    print(f"Loop Time: {time.time()-start}")
+        # plot masked image with steering angle
+        mask = cv2.resize(mask_img.astype(np.uint8)*255, (640, 480))
+        mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+        opacity = 0.3
+        cv2.addWeighted(mask, opacity, img, 1 - opacity, 0, img)
+        cv2.line(img, (curr_x_coord, 0), (curr_x_coord, 480), (0, 0, 255), 2)
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+        print(f"Loop Time: {time.time()-start}")
+    except Exception as e:
+        print(f"No data in buffer. Got {e}")
+        continue

ea4f30e97a20cb79a55be451df198df39ea73f80
Curtis Johnson <curtiscarljohnson@gmail.com>
2024-03-29 15:37:30 -0600

    updates to realsense

--- src/ECEnRacer.py
+++ src/ECEnRacer.py
@@ -30,61 +30,104 @@ from RealSense import *
 import cv2
 import pickle
 import socket
-# from simple_pid import PID
+from simple_pid import PID
+
+from kachow import CircleOfExpansionEstimator
+coeEstimator = CircleOfExpansionEstimator(display=False)
+
+# Set up PID Controller 
+pid = PID()
+pid.Ki = 0.0
+pid.Kd = 0.0
+pid.Kp = -40/320 #degrees per pixel
+frameUpdate = 1
+pid.sample_time = frameUpdate/30.0
+pid.output_limits = (-8,8)
+desXCoord = 640 * 1/3
+pid.setpoint = desXCoord
+
 
 # set up the camera
-rs = RealSense(RS_VGA)
-(time_, rgb, depth, accel, gyro) = rs.getData()
+rs = RealSense(RS_VGA, enable_depth=True)
+rgb = rs.getData()
 print("Camera is ready...")
 
 # set up the Car
-# Car = Arduino("/dev/ttyUSB0", 115200)                # Linux
-# time.sleep(3)
-# Car.zero(1440)      # Set car to go straight.  Change this for your car.
-# Car.pid(1)          # Use PID control
-# print("Car is ready...")
+Car = Arduino("/dev/ttyUSB0", 115200)                # Linux
+time.sleep(3)
+Car.zero(1400)      # Set car to go straight.  Change this for your car.
+Car.pid(1)          # Use PID control
+print("Car is ready...")
+
+# coeEstimator = CircleOfExpansionEstimator(display=True)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1000000)
-s.settimeout(0.25)
-server_ip = "10.32.114.243"
-server_port = 5555
-print("Client is ready...")
+receive_buffer_size = 72
+s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 3000)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, receive_buffer_size)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setblocking(False)
 
-# Car.drive(3.0)
+s.settimeout(0.15)
+server_ip = "10.37.85.80"
+# server_ip = "10.32.114.243"
+server_port = 5555
+s.connect((server_ip, server_port))
+print("Client is connected to server...")
+
+time.sleep(2.0)
+
+Car.drive(2.5)
 
 while(True):
 	loopStart = time.time()
-	(time_, img, _, accel, gyro) = rs.getData()
+	img = rs.getData()
 
 
 	# encode the image
+	# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	img = cv2.resize(img, (160, 120))
+	# print(img.shape)
 	ret, buffer = cv2.imencode(".jpg", img, [int(cv2.IMWRITE_JPEG_QUALITY), 50])
 	x_as_bytes = pickle.dumps(buffer)
-
+	print(x_as_bytes.__sizeof__())
 
 	# Send image to server
 	try:
-		s.sendto((x_as_bytes), (server_ip, server_port))
+		# print(x_as_bytes.__sizeof__())
+		# s.sendto((x_as_bytes), (server_ip, server_port))
+		s.sendall(x_as_bytes)
 	except:
 		print("Timeout sending data to server. Continuing")
+		Car.drive(0.)
+		# cv2.imwrite('depth.jpg', depth)
 		# Car.steer(0.0)
+		# Car.drive(1.5)
 		continue
 
 
 	# Get steering command from server
 	try:
-		server_response = s.recvfrom(1000000)
+		server_response = s.recvfrom(receive_buffer_size)
 		steer_command = int(server_response[0])
+		Car.drive(2.5)
 		print(steer_command)
-	except:
-		print("Timeout waiting for server response. Sending another message")
-		# Car.steer(0.0)
+	except Exception as e:
+		# print(f"Timeout waiting for server response. Sending another message. Got {e}")
+
+		start = time.time()
+
+		#get steering angle from depth instead
+		# depth = rs.getData(enable_depth=True, enable_rgb=False)
+		Car.drive(0.)
+
+		print(f"Timeout. SLOWING DOWN. Time to get depth: {time.time() - start}")
+		
 		continue
 
 
 	# Do some steering
-	# Car.steer(steer_command)
+	Car.steer(steer_command)
 	
 
 	print(f"Loop Time: {time.time()- loopStart} s")
--- src/RealSense.py
+++ src/RealSense.py
@@ -33,11 +33,11 @@ class RealSense:
         config = rs.config()
 
         config.enable_stream(rs.stream.color, rgbSize[0], rgbSize[1], rs.format.bgr8, 60)
-        if enable_depth:
-            config.enable_stream(rs.stream.depth, depthSize[0], depthSize[1], rs.format.z16, 60)  
-        self.enable_depth = enable_depth
-        config.enable_stream(rs.stream.accel, rs.format.motion_xyz32f, 250)
-        config.enable_stream(rs.stream.gyro, rs.format.motion_xyz32f, 200)
+        config.enable_stream(rs.stream.depth, depthSize[0], depthSize[1], rs.format.z16, 60)  
+        # if enable_depth:
+        # self.enable_depth = enable_depth
+        # config.enable_stream(rs.stream.accel, rs.format.motion_xyz32f, 250)
+        # config.enable_stream(rs.stream.gyro, rs.format.motion_xyz32f, 200)
 
         # Start streaming
         prof = self.pipeline.start(config)
@@ -58,29 +58,35 @@ class RealSense:
     def accel_data(self, accel):
         return np.asarray([accel.x, accel.y, accel.z])
 
-    def getData(self):
+    def getData(self, enable_depth=False, enable_rgb=True):
         # start realsense pipeline
         rsframes = self.pipeline.wait_for_frames()
-        color_frame = rsframes.get_color_frame()
-        rgb = np.asanyarray(color_frame.get_data())
+        if enable_rgb:
+            color_frame = rsframes.get_color_frame()
+            rgb = np.asanyarray(color_frame.get_data())
+            # frame = rgb
         # iterate through camera/IMU data, updating global variable
         for rsframe in rsframes:
             # Retrieve IMU data
             if rsframe.is_motion_frame():
-                accel = self.accel_data(rsframe.as_motion_frame().get_motion_data())
-                gyro = self.gyro_data(rsframe.as_motion_frame().get_motion_data())
+                pass
+                # accel = self.accel_data(rsframe.as_motion_frame().get_motion_data())
+                # gyro = self.gyro_data(rsframe.as_motion_frame().get_motion_data())
             # Retrieve depth data
             if rsframe.is_depth_frame():
-                rsframes = self.align.process(rsframes)
-                # Update color and depth frames:ss
-                depth_frame = rsframes.get_depth_frame()
-                # Convert to numpy array
-                depth = cv2.normalize(~np.asanyarray(depth_frame.get_data()), None, 255, 0, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-                #depth = np.asanyarray(self.colorizer.colorize(depth_frame).get_data())
-
-        if not self.enable_depth:
-            return (time.time(), rgb, None, accel, gyro)
-        return(time.time(), rgb, depth, accel, gyro)
+                if enable_depth:
+                    rsframes = self.align.process(rsframes)
+                    # Update color and depth frames:ss
+                    depth_frame = rsframes.get_depth_frame()
+                    # Convert to numpy array
+                    depth = cv2.normalize(~np.asanyarray(depth_frame.get_data()), None, 255, 0, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+                    # frame = depth
+                    # depth = np.asanyarray(self.colorizer.colorize(depth_frame).get_data())
+        return rgb,depth
+        # if not enable_depth:
+        #     return (time.time(), rgb, None, None, None)
+        # # return(time.time(), rgb, depth, accel, gyro)
+        # return(time.time(), rgb, depth, None, None)
 
 
 
